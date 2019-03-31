4189e914e61343a37f4352ccb18c04e85a628e84
Muhammad Arif Setiyawan <muhamadarief251@gmail.com>
2019-04-01 06:08:31 +0700

    add cors

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const app = express();
 
 // parse requests
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
